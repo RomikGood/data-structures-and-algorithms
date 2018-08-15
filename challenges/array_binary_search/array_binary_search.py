4739f7ec1b9fad73dacd77f0cec62b0a2d2e460f
Roman Kireev <kireev@gmail.com>
2018-08-15 16:10:51 -0700

    binary search finished

---
+++ challenges/array_binary_search/array_binary_search.py
@@ -0,0 +1,28 @@
+def binary_search(arr, key):
+    start = 0
+    end = len(arr) - 1
+    
+    while start <= end:
+ 
+        mid = (start + end) // 2
+         
+        # Check if key == mid
+        if arr[mid] == key:
+            return mid
+            
+ 
+        # If ket is greater, ignore left half
+        elif arr[mid] < key:
+            start = mid + 1
+ 
+        # If key is smaller, ignore right half
+        elif arr[mid] > key:
+            end = mid - 1
+        
+    return -1
+     
+
+    
+arr = [1,4,6,8,14,45]
+key = 11
+print(binary_search(arr, key))
