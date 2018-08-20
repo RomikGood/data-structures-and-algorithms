4c6f1da7a56684efb1004fa7f856bca012a2f170
Roman Kireev <kireev@gmail.com>
2018-08-20 14:49:29 -0700

    fixed Node classe file and tests

--- challenges/array_binary_search/test_array_binary_search.py
+++ challenges/array_binary_search/test_array_binary_search.py
@@ -1,20 +1,24 @@
 from .array_binary_search import binary_search
 import pytest
 
+
 def test_even_binary_search():
     expected = 2
     actual = binary_search([1,2,3,4], 3)
     assert expected == actual
 
+
 def test_binary_search():
     expected = 4
-    actual = binary_search([1,2,3,4,11], 11)
+    actual = binary_search([1, 2, 3, 4, 11], 11)
     assert expected == actual
+
 
 def test_no_key_exist_binary_search():
     expected = -1
-    actual = binary_search([1,2,3,4,11], 5)
+    actual = binary_search([1, 2, 3, 4, 11], 5)
     assert expected == actual
+
 
 def test_binary_search_exists():
     assert binary_search
