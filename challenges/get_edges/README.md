1148dd0d99ce026482b8f768c0d981bc1fe5b3f3
Roman Kireev <kireev@gmail.com>
2018-09-23 23:50:58 -0700

    added graphs

---
+++ challenges/get_edges/README.md
@@ -0,0 +1,16 @@
+## API
+no API
+
+## Change Log
+
+
+09-18-2018 10pm - Created a class called AnimalShelter which holds only dogs and cats. The shelter operates using a first-in, first-out approach.
+
+08-29-2018 created tests for animal shelter class
+
+## Image
+![alt text]()
+
+## License
+This project is licensed under the MIT license
+-->
--- data_structures/graph/graph.py
+++ data_structures/graph/graph.py
@@ -1,35 +1,51 @@
 class Graph:
     def __init__(self):
         self.graph = {}
+        self.edge = []
+        self.weight = {}
 
     def __repr__(self):
-        pass
+        return list(self.graph.keys())
 
     def __str__(self):
-        pass
+        return list(self.graph.keys())
 
     def __len__(self):
-        pass
+        return len(list(self.graph.keys()))
 
     def add_vert(self, val):
+        """if val is not it self.graph dict add key 'val' with 
+            empty list to the dict
         """
-        """
-        # add vertice to self.graph
-        # check to see if the vert already exists: if so raise exception
-        # create a helper method
+        if val not in self.graph:
+            self.graph[val] = []
+        else:
+            raise ValueError
 
+
+    
     def has_vert(self, val):
+        """function checks if val in graph dict
         """
-        """
-        # checks for a key in the graph
+        if val in self.graph:
+            return True
+        else:
+            return False
+        
 
     def add_edge(self, v1, v2, weight):
+        """add weight to our weight dictionary
         """
-        """
+        if v1 in self.graph and v2 in self.graph:
+            self.weight[(v1, v2)] = weight
+        else:
+            raise ValueError
+
         # add a relationship and weight between two verts
         # don't forget to validate
 
     def get_neighbors(self, val):
+        """returns list with adjacent vert
         """
-        """
+        return self.graph[val]
         # Given a val (key), return all all adjacent verts
